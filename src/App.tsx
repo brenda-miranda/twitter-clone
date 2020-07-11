d24d7067615b27dfd5fa95fa5240bdcb22e27c33
Brenda Miranda <brendamndx@gmail.com>
2020-07-11 02:48:57 -0300

    Add Layout and GlobalStyles to App.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import GlobalStyles from './styles/GlobalStyles';
+import Layout from './components/Layout';
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <>
+     <Layout />
+
+      <GlobalStyles />
+    </>
   );
 }
 
