5e75c1343a0b3e6a622d5b49917976fd0534be49
Brenda Miranda <brendamndx@gmail.com>
2020-07-11 02:50:06 -0300

    Create Layout component

---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Main from '../Main';
+import { Container, Wrapper } from './styles';
+
+const Layout: React.FC = () => {
+   return (
+      <Container>
+         <Wrapper>
+            {/* <MenuBar /> */}
+            <Main />
+            {/* <SideBar /> */}
+         </Wrapper>
+      </Container>
+   )
+
+}
+
+export default Layout;
