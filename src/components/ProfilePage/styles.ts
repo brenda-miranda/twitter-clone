8113716da77f6025da0b39ed112b812b86cac747
Brenda Miranda <brendamndx@gmail.com>
2020-07-12 14:04:23 -0300

    Add styles to EditButton in ProfilePage

--- src/components/ProfilePage/styles.ts
+++ src/components/ProfilePage/styles.ts
@@ -1,5 +1,6 @@
 import styled, { css } from 'styled-components';
 import { LocationOn, Cake } from '../../styles/Icons';
+import Button from '../Button';
 
 export const Container = styled.div`
    display: flex;
@@ -88,5 +89,19 @@ export const Followage = styled.div`
       & + span {
          margin-right: 20px;
       }
+   }
+`;
+
+export const EditButton = styled(Button)`
+   position: absolute;
+   top: 2vw;
+   right: 7px;
+   padding: 4px 16px;
+   font-size: 16px;
+
+   @media (min-width: 320px) {
+      top: 10px;
+      padding: 10px 19px;
+      font-size: 15px;
    }
 `;
