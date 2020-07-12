01901617390799cdaf549e4312d54ace11204145
Brenda Miranda <brendamndx@gmail.com>
2020-07-12 18:18:25 -0300

    Import MenuBar component to Layout

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Main from '../Main';
+import MenuBar from "../MenuBar";
 import { Container, Wrapper } from './styles';
 
 const Layout: React.FC = () => {
    return (
       <Container>
          <Wrapper>
-            {/* <MenuBar /> */}
+            <MenuBar />
             <Main />
             {/* <SideBar /> */}
          </Wrapper>
